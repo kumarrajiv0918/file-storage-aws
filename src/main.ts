d09deaaf2f27000e05f15980d6cbbba65e3b1517
Mukut Prasad <mukutprasad1996@gmail.com>
2025-05-20 11:08:32 +0530

    Push existing code

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './appModule';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(process.env.PORT || 5000);
+}
+
+bootstrap();
+
---
+++ src/module/file/service/fileUploadService.ts
@@ -0,0 +1,48 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { s3 } from '../../../config/awsConfig';
+import {
+    AWSBucketNameIsNotDefinedInEnvironmentVariables,
+    fileUloadedSuccessfully
+} from '../common/message/messageFileUpload';
+
+@Injectable()
+export class FileUploadService {
+    async uploadFile(file: Express.Multer.File) {
+        const bucket = process.env.AWS_BUCKET_NAME;
+
+        if (!bucket) {
+            return {
+                status: false,
+                message: AWSBucketNameIsNotDefinedInEnvironmentVariables
+            }
+        }
+
+        const params = {
+            Bucket: bucket,
+            Key: `${Date.now()}-${file.originalname}`,
+            Body: file.buffer,
+            ContentType: file.mimetype,
+            // ACL: 'public-read', ❌ Remove this
+        };
+
+
+        try {
+            const result = await s3.upload(params).promise();
+            return {
+                status: true,
+                message: fileUloadedSuccessfully,
+                data: {
+                    url: result.Location,
+                    key: result.Key,
+                    size: file.size,
+                    mimetype: file.mimetype,
+                }
+            };
+        } catch (error: any) {
+            return {
+                status: false,
+                message: error.message
+            }
+        }
+    }
+}
